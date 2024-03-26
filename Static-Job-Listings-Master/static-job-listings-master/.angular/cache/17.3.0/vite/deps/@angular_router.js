b786d9e8723a6eaab12d9df92bbaf0e8a7e48fba
NatiDelta <natalia.piorkowska@seargin.com>
2024-03-26 22:26:24 +0100

    get data service created

--- Static-Job-Listings-Master/static-job-listings-master/.angular/cache/17.3.0/vite/deps/@angular_router.js
+++ Static-Job-Listings-Master/static-job-listings-master/.angular/cache/17.3.0/vite/deps/@angular_router.js
@@ -1,13 +1,15 @@
 import {
+  Title
+} from "./chunk-ZAVF6UPK.js";
+import {
   DOCUMENT,
   HashLocationStrategy,
   LOCATION_INITIALIZED,
   Location,
   LocationStrategy,
   PathLocationStrategy,
-  Title,
   ViewportScroller
-} from "./chunk-ISOS2PAI.js";
+} from "./chunk-TQGDOKBN.js";
 import {
   APP_BOOTSTRAP_LISTENER,
   APP_INITIALIZER,
--- Static-Job-Listings-Master/static-job-listings-master/src/app/app.component.ts
+++ Static-Job-Listings-Master/static-job-listings-master/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from "./card/card/card.component";
+import { CardComponent } from './card/card/card.component';
+import { Card } from './domain/card';
+import { JobsService } from './services/jobs.service';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, CardComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [CardComponent],
 })
 export class AppComponent {
   title = 'static-job-listings-master';
+  data!: Card[];
+
+  constructor(private jobService: JobsService) {}
+
+  ngOnInit() {
+    this.jobService.getData().subscribe((jobs) => {
+      this.data = jobs;
+    });
+  }
 }
