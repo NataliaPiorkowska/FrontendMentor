5c19d545a1343ad0567d3be6806d2d7856f9b479
NatiDelta <natalia.piorkowska@seargin.com>
2024-03-29 08:24:09 +0100

    filter component added view of job card mobile fixed

--- Static-Job-Listings-Master/static-job-listings-master/src/app/app.component.ts
+++ Static-Job-Listings-Master/static-job-listings-master/src/app/app.component.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
-import { CardComponent } from './card/card/card.component';
 import { Card } from './domain/card';
 import { JobsService } from './services/jobs.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { CardComponent } from './components/card/card.component';
+import { FilterComponent } from './components/filter/filter/filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CardComponent,NgFor],
+  imports: [CardComponent, NgFor, FilterComponent, NgIf],
 })
 export class AppComponent {
   title = 'static-job-listings-master';
   data!: Card[];
+  filterComponent: boolean = false;
+  filterItems: string[] = [];
 
   constructor(private jobService: JobsService) {}
 
   ngOnInit() {
     this.jobService.getData().subscribe((jobs) => {
       this.data = jobs;
-      console.log(this.data)
     });
+  }
+
+  addFilter(newFilter: string) {
+    this.filterComponent = true;
+    this.filterItems.push(newFilter);
+  }
+
+  removeFilter(filterItem: string) {
+    const index = this.filterItems.indexOf(filterItem);
+    if (index > -1) {
+      this.filterItems.splice(index, 1);
+    }
+    if (this.filterItems.length == 0) {
+      this.filterComponent = false;
+    }
   }
 }
