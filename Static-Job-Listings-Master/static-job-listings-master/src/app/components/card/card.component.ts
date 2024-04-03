a8aadf8b97019ca38597ddce30e478c93f84485d
NatiDelta <natalia.piorkowska@seargin.com>
2024-04-03 21:55:51 +0200

    fixed in filter creation function, added some visuals

--- Static-Job-Listings-Master/static-job-listings-master/src/app/components/card/card.component.ts
+++ Static-Job-Listings-Master/static-job-listings-master/src/app/components/card/card.component.ts
@@ -1,20 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Card } from '../../domain/card';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, NgStyle],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
   @Input() cardData!: Card;
   @Output() newFilterEvent = new EventEmitter<string>();
-  ngOnInit() {
-
-  }
+  isHovered: boolean = false;
 
   setFilter(item: string) {
     this.newFilterEvent.emit(item);
